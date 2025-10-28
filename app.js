23183c0c5b2bc62b6e3e35f1d80bc0d7397dfdd8
nordtwig <nordqvist.gaming@gmail.com>
2025-10-28 21:50:08 +0100

    add tech stack tab

--- app.js
+++ app.js
@@ -8,7 +8,62 @@ var projects = [
 		]
 	}
 ]
-
+var techStack = [
+	{
+		name: "Unity",
+		description: "Game Engine, using C# as main programming language.",
+		url: "https://unity.com/",
+		logo: "images/logos/unity_logo.png",
+	},
+	{
+		name: "Godot",
+		description: "Game Engine, using GDScript as main programming language.",
+		url: "https://godotengine.org/",
+		logo: "images/logos/godot_logo.png",
+	},
+	{
+		name: "Blender",
+		description: "3D Modelling and animation software.",
+		url: "https://www.blender.org/",
+		logo: "images/logos/blender_logo.png",
+	},
+	{
+		name: "Audacity",
+		description: "Audio recording and editing software.",
+		url: "https://www.audacityteam.org/",
+		logo: "images/logos/audacity_logo.png",
+	},
+	{
+		name: "Visual Studio Code",
+		description: "Code editor for various programming languages.",
+		url: "https://code.visualstudio.com/",
+		logo: "images/logos/code_logo.png",
+	},
+	{
+		name: "GitHub",
+		description: "Code hosting platform for version control and collaboration.",
+		url: "https://github.com/",
+		logo: "images/logos/github_logo.jpg",
+	},
+	{
+		name: "Krita",
+		description: "Digital painting and illustration software.",
+		url: "https://krita.org/",
+		logo: "images/logos/krita_logo.png",
+	},
+	{
+		name: "Material Maker",
+		description: "Procedural texture generation software.",
+		url: "https://www.materialmaker.org/",
+		logo: "images/logos/material_maker_logo.png",
+	},
+	{
+		name: "Reaper",
+		description: "Digital audio workstation for recording, editing, and mixing audio.",
+		url: "https://www.reaper.fm/",
+		logo: "images/logos/reaper_logo.png",
+	}
+]
 var about = [
 	{
 		name: "Noah",
@@ -22,6 +77,8 @@ function loadProjects() {
 	document.title = "👃 What am I doing?";
 	container.innerHTML = '';
 	projects.forEach(project => {
+		let card = document.createElement('div');
+		card.classList.add('container-card');
 		let projectDiv = document.createElement('div');
 		projectDiv.classList.add('project');
 		let title = document.createElement('h2');
@@ -45,7 +102,40 @@ function loadProjects() {
 		projectDiv.appendChild(title);
 		projectDiv.appendChild(description);
 		projectDiv.appendChild(link);
-		container.appendChild(projectDiv);
+		card.appendChild(projectDiv);
+		container.appendChild(card);
+	});
+}
+
+function loadTechStack() {
+	let container = document.querySelector('.container-body');
+	document.title = "👃 How am I doing it?";
+	container.innerHTML = '';
+	techStack.forEach(tech => {
+		let card = document.createElement('div');
+		card.classList.add('container-card');
+		let techDiv = document.createElement('div');
+		techDiv.classList.add('tech-item');
+		let logo = document.createElement('img');
+		logo.src = tech.logo;
+		logo.classList.add('tech-logo');
+		let name = document.createElement('h2');
+		name.textContent = tech.name;
+		let description = document.createElement('p');
+		description.textContent = tech.description;
+		let link = document.createElement('a');
+		link.target = "_blank";
+		link.href = tech.url;
+		link.textContent = "Learn More";
+		techDiv.appendChild(logo);
+		techDiv.appendChild(name);
+		techDiv.appendChild(description);
+		techDiv.appendChild(link);
+		card.appendChild(techDiv);
+		container.appendChild(card);
+		let divider = document.createElement('div');
+		divider.classList.add('container-divider');
+		container.appendChild(divider);
 	});
 }
 
@@ -54,6 +144,8 @@ function loadAbout() {
 	document.title = "👃 Who am I?";
 	container.innerHTML = ''; 
 	about.forEach(person => {
+		let card = document.createElement('div');
+		card.classList.add('container-card');
 		let personDiv = document.createElement('div');
 		personDiv.classList.add('about-person');
 		let name = document.createElement('h2');
@@ -62,7 +154,8 @@ function loadAbout() {
 		description.textContent = person.description;
 		personDiv.appendChild(name);
 		personDiv.appendChild(description);
-		container.appendChild(personDiv);
+		card.appendChild(personDiv);
+		container.appendChild(card);
 	}) 
 }
 
